bd409ad053be4f9ddfc83d2ce74f62a63fb9433f
Priscila <pguillen.oliv@gmail.com>
2025-05-12 09:51:24 +0200

    first commit

---
+++ src/main/java/com/rootsy/Rootsy/RootsyApplication.java
@@ -0,0 +1,13 @@
+package com.rootsy.Rootsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RootsyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rootsy/Rootsy/RootsyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rootsy.Rootsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RootsyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
