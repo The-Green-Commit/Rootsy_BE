8b3e10ce31cc4b0c701c262249dbe91242e1e685
Priscila <pguillen.oliv@gmail.com>
2025-05-15 18:09:29 +0200

    add basic repo and controller for GENUS entity

---
+++ src/main/java/com/rootsy/Rootsy/repository/GenusRepository.java
@@ -0,0 +1,9 @@
+package com.rootsy.Rootsy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rootsy.Rootsy.model.Genus;
+
+public interface GenusRepository extends JpaRepository<Genus, Integer>{
+
+}
