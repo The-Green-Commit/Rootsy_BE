c430d7ec0a6b5d40c36b1cdb2655aa969527b006
Priscila <pguillen.oliv@gmail.com>
2025-05-15 14:55:25 +0200

    add basic Repo and Controller for Type entity

--- src/main/java/com/rootsy/Rootsy/controller/TypeController.java
+++ src/main/java/com/rootsy/Rootsy/controller/TypeController.java
@@ -1,5 +1,22 @@
 package com.rootsy.Rootsy.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/type")
 public class TypeController {
     
+//private final TypeService typeService;
+
+//public TypeController(TypeService typeService){
+//  this.typeService = typeService;
+//}
+
+//MAP GET ALL
+
+//MAP GET BY TYPE
+
+//MAP POST
+
 }
