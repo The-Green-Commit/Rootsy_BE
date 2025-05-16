0a4cbae99b7af2710c06bfe6601f89810d98cdf6
Priscila <pguillen.oliv@gmail.com>
2025-05-16 09:54:49 +0200

    add CREATE type, add file for future exception handling

--- src/main/java/com/rootsy/Rootsy/controller/TypeController.java
+++ src/main/java/com/rootsy/Rootsy/controller/TypeController.java
@@ -2,21 +2,34 @@ package com.rootsy.Rootsy.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.rootsy.Rootsy.service.TypeService;
+import com.rootsy.Rootsy.model.Type;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("api/v1/type")
 public class TypeController {
     
-private final TypeService typeService;
+    private final TypeService typeService;
 
-public TypeController(TypeService typeService){
- this.typeService = typeService;
-}
+    public TypeController(TypeService typeService){
+    this.typeService = typeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createType(@Valid @RequestBody Type type){
+        return this.typeService.createType(type);
+    }
+
+    }
+    
 
 //MAP GET ALL
 
 //MAP GET BY TYPE
 
-//MAP POST
 
-}
--- src/main/java/com/rootsy/Rootsy/service/TypeService.java
+++ src/main/java/com/rootsy/Rootsy/service/TypeService.java
@@ -1,7 +1,10 @@
 package com.rootsy.Rootsy.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.rootsy.Rootsy.model.Type;
 import com.rootsy.Rootsy.repository.TypeRepository;
 
 @Service
@@ -11,6 +14,15 @@ public class TypeService {
 
     public TypeService(TypeRepository typeRepository){
         this.typeRepository = typeRepository;
+    }
+
+    public ResponseEntity<Object> createType(Type type) {
+
+        // if (typeRepository.findByType(type.getType()) != null) {
+        //     throw new TypeAlreadyExistsException(message: "(!) ERROR: this type already exists. Try a new one.")
+        // }
+
+        return new ResponseEntity<>(typeRepository.save(type), HttpStatus.CREATED);
     }
 
 }
