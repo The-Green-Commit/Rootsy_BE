52c05e547c30c5612826815153d75a709570b7b8
Priscila <pguillen.oliv@gmail.com>
2025-05-16 10:09:26 +0200

    add service layer and controller service reference

--- src/main/java/com/rootsy/Rootsy/controller/FamilyController.java
+++ src/main/java/com/rootsy/Rootsy/controller/FamilyController.java
@@ -2,23 +2,20 @@ package com.rootsy.Rootsy.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.rootsy.Rootsy.service.FamilyService;
 
 @RestController
 @RequestMapping("api/v1/family")
 public class FamilyController {
     
-    //private final FamilyService familyService;
+    private final FamilyService familyService;
     
-    //public FamilyController(FamilyService familyService){
-    //  this.familyService = familyService;
-    //}
+    public FamilyController(FamilyService familyService){
+     this.familyService = familyService;
+    }
     
     //MAP GET ALL
     
-    //MAP GET BY FAMILY
-    
     //MAP POST
-
-    //MAP DELETE(sin cascade)
 
 }
--- src/main/java/com/rootsy/Rootsy/service/FamilyService.java
+++ src/main/java/com/rootsy/Rootsy/service/FamilyService.java
@@ -1,5 +1,13 @@
 package com.rootsy.Rootsy.service;
 
+import com.rootsy.Rootsy.repository.FamilyRepository;
+
 public class FamilyService {
     
+    private final FamilyRepository familyRepository;
+
+    public FamilyService(FamilyRepository familyRepository) {
+        this.familyRepository = familyRepository;
+    }
+
 }
